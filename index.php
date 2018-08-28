22be905326b933de6b60499e1971b074439a5963
HarshShah27 <hsgotu27@gmail.com>
2018-08-28 20:07:43 +0530

    rakshabandhan Website Work

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Title </title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+
+
+</head>
+
+<body>
+    <header class="menu">
+        <nav>
+            <ul>
+                <li><a target="#home">Home</a></li>
+                <li><a>History</a></li>
+                <li><a>Gallary</a></li>
+                <li><a class="fab fa-facebook-square"></a></li>
+                <li><a class="fab fa-instagram"></a></li>
+            </ul>
+        </nav>
+
+    </header>
+
+    <section class="home" id="home">
+
+        <div class="center">
+            <div class="left"></div>
+            <div class="right">
+                <p>The festival is made up of two words, namely "Raksha" and "Bandhan." As per the Sanskrit terminology, the occasion means "the tie or knot of protection" where "Raksha" stands for the protection and "Bandhan" signifies the verb to tie. Together, the festival symbolizes the eternal love of brother-sister relationship which does not mean just the blood relationships only. It is also celebrated among cousins, sister and sister-in-law (Bhabhi), fraternal aunt (Bua) and nephew (Bhatija) and other such relations.The festival of Raksha Bandhan is known to have originated centuries before and there are several stories related to the celebration of this special festival. Some of the various accounts related to the Hindu mythology are described below:
+
+
+
+                </p>
+            </div>
+        </div>
+    </section>
+    <section class="history">
+
+        <h1>HELLO</h1>
+        <h1>HELLO</h1>
+        <h1>HELLO</h1>
+        <h1>HELLO</h1>
+        <h1>HELLO</h1>
+    </section>
+</body>
+
+</html>
